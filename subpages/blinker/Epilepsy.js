22ac452e605f0e965d508a0c8736463f25d4a0f1
Reed Krawiec <reedkrawiec@gmail.com>
2016-04-17 08:30:50 -0400

    six hundo

--- subpages/blinker/Epilepsy.js
+++ subpages/blinker/Epilepsy.js
@@ -40,8 +40,8 @@ console.log(f);
         $("html").css("background-color","blue");
         $("#number").css("display","none");
       }
-    },700);
-  },700);
+    },600);
+  },600);
 }
 
 function FromBinary(y) {
