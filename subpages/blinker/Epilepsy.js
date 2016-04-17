7347dbf7bbaa5736ec90a385b5e7734f797403e1
Reed Krawiec <reedkrawiec@gmail.com>
2016-04-17 09:30:58 -0400

    150

--- subpages/blinker/Epilepsy.js
+++ subpages/blinker/Epilepsy.js
@@ -40,8 +40,8 @@ console.log(f);
         $("html").css("background-color","blue");
         $("#number").css("display","none");
       }
-    },100);
-  },100);
+    },150);
+  },150);
 }
 
 function FromBinary(y) {
